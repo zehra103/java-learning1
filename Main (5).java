1d066a8cf24acd020827b3d92c5c14186c706dbd
zehra103 <narrahema4@gmail.com>
2025-01-16 19:22:08 +0530

    Add files via upload

---
+++ Main (5).java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+    {
+        public static void main(String[] args)
+        {
+            int s=6,area;
+            area=s*s;
+            
+            System.out.println("side is:"+s);
+            System.out.println("area of the square is :"+area);
+        }
+    }
