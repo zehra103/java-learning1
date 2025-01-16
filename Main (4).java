f587ff056e1a55ea76ddda96ea9af6ab8ac19571
zehra103 <narrahema4@gmail.com>
2025-01-16 19:07:40 +0530

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+ public static void main(String[] args)
+ {
+    
+    String name = "zehra";
+    String  group = "cse";
+    int id=40;
+    int fee=75000;
+    double gpa=8.9;
+    
+    System.out.println("Student name: " + name);
+    System.out.println("Student group:" +group);
+    System.out.println("Student id: " + id);
+    System.out.println("Student fee: " + fee);
+    System.out.println("Student gpa: " + gpa);
+  }
+}
