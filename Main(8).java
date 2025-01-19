c1a16b15e5504e6f35e9e7243daca4f31df1281a
zehra103 <narrahema4@gmail.com>
2025-01-19 21:50:56 +0530

    Add files via upload

---
+++ Main(8).java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    String t="going to home";
+	    System.out.println(t.indexOf("home"));
+	    
+	}
+}
+	
