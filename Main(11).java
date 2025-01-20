396e87785f685436c5121581f48e83cd06daaa13
zehra103 <narrahema4@gmail.com>
2025-01-20 22:16:01 +0530

    Add files via upload

---
+++ Main(11).java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	  int x = 20;
+	  int y = 40;
+	  int myAge = 21;
+	  int votingAge = 18;
+	  System.out.println(x>y);
+	  System.out.println(x==20);
+	  System.out.println(10==500);
+	  if(myAge>=votingAge)
+	  System.out.println("can vote");
+	  else
+	  System.out.println("cannot vote");
+	}
+}
