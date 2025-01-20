cac81e4b9dd6bdf3afe9510056315529dc42cfbb
zehra103 <narrahema4@gmail.com>
2025-01-20 22:07:28 +0530

    Add files via upload

---
+++ Main(10).java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int randomNum = (int)(Math.random() * 51);
+		System.out.println(Math.max(5,8));
+		System.out.println(Math.min(7,9));
+		System.out.println(Math.sqrt(64));
+		System.out.println(Math.abs(-6.333));
+		System.out.println(Math.random());
+		System.out.println(randomNum);
+	}
+}
