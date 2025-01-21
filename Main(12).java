3f25c3d9227b3bae0721d38e145d3359d8a8ebd3
zehra103 <narrahema4@gmail.com>
2025-01-21 21:29:37 +0530

    Add files via upload

---
+++ Main(12).java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+	 int x=9;
+	 int y=45;
+     
+     if(x>y)
+     System.out.println("x is greater than y");
+     else
+	 System.out.println("y is greater than x");
+	}
+}
