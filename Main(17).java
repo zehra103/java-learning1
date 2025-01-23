6fb6873bd32588a0b0c5a9467759ab47e373a2da
zehra103 <narrahema4@gmail.com>
2025-01-23 21:12:42 +0530

    Add files via upload

---
+++ Main(17).java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	for (int i=0;i<=100; i=i+2) 
+	{
+      System.out.println(i);
+    }
+  }
+}
