b16237ace9422593e4c1f2b8998f4a87d934be39
zehra103 <narrahema4@gmail.com>
2025-01-23 20:57:30 +0530

    Add files via upload

---
+++ Main(15).java
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) 
+	{
+    int month = 9;
+    switch (month) 
+    {
+      case 1:System.out.println("jan");
+        break;
+      case 2:System.out.println("feb");
+        break;
+      case 3:System.out.println("mar");
+        break;
+      case 4: System.out.println("april");
+        break;
+      case 5:System.out.println("may");
+        break;
+      case 6: System.out.println("june");
+        break;
+      case 7:System.out.println("july");
+        break;
+      case 8:System.out.println("aug");
+        break;
+      case 9:System.out.println("sept");
+        break;
+      case 10: System.out.println("oct");
+        break;
+      case 11:System.out.println("nov");
+        break;
+      case 12: System.out.println("dec");
+        break;
+      default:System.out.println("ERROR");
+        break;
+    }
+    }
+}
