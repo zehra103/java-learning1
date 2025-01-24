7311cedc1edca2d9513d1074384e6dadad6c917a
zehra103 <narrahema4@gmail.com>
2025-01-24 20:06:49 +0530

    Add files via upload

---
+++ Main(20).java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	 String[] cars = {"FERRARI", "BMW", "LAMBORGINI", "AUDI","MERCEDES","FORD","AUDI"};
+     System.out.println(cars.length);
+  }
+}
+
