7ea487537d911eb0b3c81c55662ed453be26f2e2
zehra103 <narrahema4@gmail.com>
2025-01-26 21:09:48 +0530

    Add files via upload

---
+++ Main(22).java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+public class Main 
+{
+  public static void main(String[] args) 
+  {
+    int i = 0;
+    while (i < 20) 
+    {
+      System.out.println(i);
+      i++;
+      if (i == 10) 
+       break;
+    }  
+  }
+}
