54c10216f262e5f407732d3ee38a07781d4edc25
zehra103 <narrahema4@gmail.com>
2025-01-29 20:50:37 +0530

    Add files via upload

---
+++ Main(26).java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main 
+{
+  static void myMethod() 
+  {
+    System.out.println("HEY!");
+  }
+   public static void main(String[] args) 
+  {
+    myMethod();
+  }
+}
